2d05a61ce2fa77b4b606194283ade672a2f3b8b0
sebastianlerner <sl7ey@virginia.edu>
2016-04-29 03:18:33 -0400

    merge

--- app/src/main/java/com/example/sebastianlerner/finalproject/CarpoolRequest.java
+++ app/src/main/java/com/example/sebastianlerner/finalproject/CarpoolRequest.java
@@ -22,7 +22,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 /**
@@ -50,18 +52,18 @@ public class CarpoolRequest extends AppCompatActivity {
 
     public void photo(View v) throws IOException {
 
-        // Save a file: path for use with ACTION_VIEW intents
-
         Intent imageIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
 
 //folder stuff
         File imagesFolder = new File(Environment.getExternalStorageDirectory(), "MyImages");
         imagesFolder.mkdirs();
 
-        File image = new File(imagesFolder, "QR_" + "photo" + ".png");
+        File image = new File(imagesFolder, "QR_" + timeStamp + ".png");
         Uri uriSavedImage = Uri.fromFile(image);
 
         imageIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+        imageIntent.putExtra("file", uriSavedImage);
         startActivityForResult(imageIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 
 
@@ -71,11 +73,21 @@ public class CarpoolRequest extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
+
                 // Image captured and saved to fileUri specified in the Intent
                 Toast.makeText(this, "Image saved to:\n" +
                         data.getData(), Toast.LENGTH_LONG).show();
+                String mCurrentPhotoPath = getIntent().getStringExtra("file");
 
-                sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://" + Environment.getExternalStorageDirectory())));
+                Log.d("mCurrentPhotoPath", mCurrentPhotoPath);
+                /*
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                File f = new File(mCurrentPhotoPath);
+                Uri contentUri = Uri.fromFile(f);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+                */
+
 
 
             } else if (resultCode == RESULT_CANCELED) {
@@ -172,6 +184,31 @@ public class CarpoolRequest extends AppCompatActivity {
 
 
 return null;
+    }
+
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
 //    private void galleryAddPic() {
